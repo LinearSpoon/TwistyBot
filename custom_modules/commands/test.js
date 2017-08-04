d9b827b9f3b41997df447b772abcfd60e3d50bfc
Linear Spoon <linearality@gmail.com>
2017-08-03 23:49:17 -0400

    test

--- custom_modules/commands/test.js
+++ custom_modules/commands/test.js
@@ -1,4 +1,4 @@
-let Table = root_require('classes/Table');
+let Canvas = require('canvas');
 
 module.exports.help = {
 	name: 'test',
@@ -13,33 +13,23 @@ module.exports.params = {
 module.exports.permissions = [];
 
 module.exports.command = async function(message, params) {
+	let canvas = new Canvas(200, 200);
+	let ctx = canvas.getContext('2d');
 
-	let stats = await apis.RuneScape.lookup_player(params[0], { priority: 1 });
-	if (!stats)
-		return Discord.code_block('Player not found.');
+	ctx.font = '30px Impact';
+	ctx.rotate(.1);
+	ctx.fillText('Awesome!', 50, 100);
 
-	let hours = apis.RuneScape.ehp.main.calculate(stats, 13034431);
+	var te = ctx.measureText('Awesome!');
+	ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+	ctx.beginPath();
+	ctx.lineTo(50, 102);
+	ctx.lineTo(50 + te.width, 102);
+	ctx.stroke();
 
-	let table = new Table();
-	table.borders = true;
-	table.set_align('ccc', 'lrr');
-	
-	// table.set_min_width(13, 13, 8);
-	table.set_data_format(
-		s => s[0].toUpperCase() + s.substr(1), // capitalize skill name
-		v => isNaN(v) ? v : util.format_number(v, 0), // Pretty print exp
-		v => isNaN(v) ? v : util.format_number(v, 2) // Pretty print ttm
-	)
-
-	table.header_row = ['Skill', 'Experience', 'TTM'];
-
-	table.data_rows = apis.RuneScape.skills.map(function(skill) {
-		return [
-			skill,
-			stats[skill].xp,
-			hours[skill] == 0 ? '-' : hours[skill]
-		];
-	});
-
-	return Discord.code_block(table.to_string());
+	let e = new Discord.RichEmbed();
+	e.setTitle('test');
+	e.attachFile({ attachment: canvas.toBuffer(), name: 'image.png' });
+	e.setImage( 'attachment://image.png' );
+	return e;
 };
