ec15a57fb586174b53c10eb6796d2e7536502316
Linear Spoon <linearality@gmail.com>
2017-10-08 13:55:29 -0400

    'full' table row option

--- src/extensions/Table.js
+++ src/extensions/Table.js
@@ -80,6 +80,12 @@ class Table
 		this.div();
 	}
 
+	// Push a "full width" centered row
+	full(value)
+	{
+		this.rows.push( { full: true, data: value } );
+	}
+
 	// Pushes a separator between rows
 	div()
 	{
@@ -101,15 +107,19 @@ class Table
 	// Convert the table to a string
 	toString()
 	{
-		// Find column widths
-		let col_widths = this.rows.reduce(function(value, row) {
-			if (row.separator) { return value; }
-			return row.data.map( (cell, index) => Math.max(cell.length, value[index]) );
+		// Find column widths and total width
+		let col_widths = this.rows.reduce(function(widths, row) {
+			if (row.separator) { return widths; }
+			if (row.full) { return widths; }
+			return row.data.map( (cell, index) => Math.max(cell.length, widths[index]) );
 		}, this._min_width);
+
+		let full_width = col_widths.reduce((a, v) => a + v, 0) + 3 * this.cols - 3;
 
 		// Align and pad the data out with spaces
 		let aligned = this.rows.map(function(row) {
 			if (row.separator) { return row; }
+			if (row.full) { return row; }
 			return row.data.map( (cell, index) => align(cell, col_widths[index], row.align[index]) );
 		});
 
@@ -118,21 +128,55 @@ class Table
 			// Prepare separator row
 			let separator = col_widths.map(width => '─'.repeat(width));
 
+			let normal = '─┼─';
+			let full_above = '─┬─';
+			let full_below = '─┴─';
+			let full_above_below = '───';
+
+			let str = '';
+
+			console.log(aligned);
+
 			// Top border
-			let str = '┌─' + separator.join('─┬─') + '─┐\n';
+			if (aligned.length > 0 && aligned[0].full)
+				str += '┌─' + separator.join(full_above_below) + '─┐\n';
+			else
+				str += '┌─' + separator.join(full_above) + '─┐\n';
 
 			// Data columns
-			let columns = aligned.map(function(row) {
+			let columns = aligned.map(function(row, index) {
 				if (row.separator)
-					return '├─' + separator.join('─┼─') + '─┤';
-				else
-					return '│ ' + row.join(' │ ') + ' │';
+				{
+					let above = aligned[index-1] && aligned[index-1].full; // Is there a full row above?
+					let below = aligned[index+1] && aligned[index+1].full; // Is there a full row below?
+
+					if (above && below)
+						return '├─' + separator.join(full_above_below) + '─┤';
+					if (above)
+						return '├─' + separator.join(full_above) + '─┤';
+					if (below)
+						return '├─' + separator.join(full_below) + '─┤';
+
+					return '├─' + separator.join(normal) + '─┤';
+				}
+
+				if (row.full)
+				{
+					return '│ ' + align(row.data, full_width, 'c') + ' │';
+				}
+
+				return '│ ' + row.join(' │ ') + ' │';
 			});
 
 			str += columns.join('\n');
 
 			// Bottom border
-			return str + '\n└─' + separator.join('─┴─') + '─┘';
+			if (aligned.length > 0 && aligned[aligned.length - 1].full)
+				str += '\n└─' + separator.join(full_above_below) + '─┘';
+			else
+				str += '\n└─' + separator.join(full_below) + '─┘';
+
+			return str;
 		}
 		else
 		{
@@ -143,8 +187,10 @@ class Table
 			let columns = aligned.map(function(row) {
 				if (row.separator)
 					return separator.join(' ');
-				else
-					return ' ' + row.join('   ') + ' ';
+				if (row.full)
+					return ' ' + align(row.data, full_width, 'c') + ' ';
+
+				return ' ' + row.join('   ') + ' ';
 			});
 
 			return columns.join('\n');
